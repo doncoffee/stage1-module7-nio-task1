7aea6e315d25c6f819c26071dfe7da8b705236fd
Ilya <iliaabr5@gmail.com>
2023-01-20 18:51:25 +0300

    update previous decision

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -2,8 +2,6 @@ package com.epam.mjc.nio;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.util.stream.Collectors;
-
 
 public class FileReader {
 
@@ -14,23 +12,12 @@ public class FileReader {
         long phone = 0l;
         String[] arr = null;
         try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
-            arr = br.lines().collect(Collectors.joining(System.lineSeparator())).split("\\W+");
+            name = br.readLine().substring("name: ".length());
+            age = Integer.parseInt(br.readLine().substring("age: ".length()));
+            email = br.readLine().substring("email: ".length());
+            phone = Long.parseLong(br.readLine().substring("phone: ".length()));
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("Name")) {
-                name = arr[i + 1];
-            }
-            if (arr[i].equals("Age")) {
-                age = Integer.parseInt(arr[i + 1]);
-            }
-            if (arr[i].equals("Email")) {
-                email = arr[i + 1];
-            }
-            if (arr[i].equals("Phone")) {
-                phone = Long.parseLong(arr[i + 1]);
-            }
         }
         return new Profile(name, age, email, phone);
     }
